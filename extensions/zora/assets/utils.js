01f10c4e5bff110532f72fcaf522c33e386f0167
zchengfa <1522977814@qq.com>
2026-01-02 22:34:05 +0800

    refactor(extension): theme app extension：样式优化、移动端适配

--- extensions/zora/assets/utils.js
+++ extensions/zora/assets/utils.js
@@ -1,4 +1,4 @@
-const FETCH_BASE_URL = "https://2a0239751bf3.ngrok-free.app";
+const FETCH_BASE_URL = "https://700832bbeb36.ngrok-free.app";
 const ZORA_TOKEN = "zora_token"
 //防抖函数
 function debounce(fun,delay = 300) {
